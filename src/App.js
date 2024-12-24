be96d93c74d8dbee30486528ff531127e5f303a3
Smit Vyas <vsmit000.01@gmail.com>
2024-12-24 11:02:05 +0530

    Assignment

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="outer">
+      <div className="App">
+        <header className="App-header">
+          <h1 className="head1">React JS</h1>
+          <img src={logo} className="App-logo" alt="logo" />
+          <div className="text1"><h2>Hello World</h2></div>
+        </header>
+      </div>
+    </div>
+  );
+}
+
+export default App;
